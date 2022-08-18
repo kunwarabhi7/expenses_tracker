ee24f26d5ca21b59ac862e20d1061a02fd881917
Abhishek <abhishekparmar162@gail.com>
2022-08-18 16:04:09 +0530

    updated some code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../Display/Card'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
@@ -6,12 +6,19 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState('');
+
+    const changeTitle = () => {
+        setTitle('Abhishek')
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div></div>
+            <button onClick={changeTitle}>Update Title</button>
         </Card>
     )
 }
